919f0c603beeb647b6bf2458e36aa0162f3fb448
Jaime Neto <jaiminho.dev@gmail.com>
2025-02-04 18:14:44 -0300

    Refactor: Refatoração do código e inclusão do README.md

--- Tarefa_Temp_Clock/Tarefa_Temp_Clock.c
+++ Tarefa_Temp_Clock/Tarefa_Temp_Clock.c
@@ -2,15 +2,15 @@
 #include "pico/stdlib.h"
 #include "hardware/timer.h"
 
-#define LED_RED 13    // Canal vermelho do LED RGB
-#define LED_GREEN 11  // Canal verde do LED RGB
-#define LED_BLUE 12   // Canal azul do LED RGB (não usado)
+#define LED_RED 13    // Vermelho
+#define LED_GREEN 11  // Verde 
+#define LED_BLUE 12   //Azul 
 
-// Códigos ANSI para cores no terminal
+//Códigos ANSI para colocar cores no terminal
 #define RED "\033[1;31m"
 #define YELLOW "\033[1;33m"
 #define GREEN "\033[1;32m"
-#define RESET "\033[0m" // Reseta a cor
+#define RESET "\033[0m" 
 
 volatile int state = 0;   // Estado do semáforo
 volatile int seconds = 0; // Contador de segundos
@@ -18,39 +18,38 @@ volatile int seconds = 0; // Contador de segundos
 // Função de callback do temporizador
 bool repeating_timer_callback(struct repeating_timer *t) {
     switch (state) {
-        case 0: // Vermelho ligado
+        case 0: // Amarelo ligado (vermelho + verde)
             printf("\nMudança de estado: " YELLOW "AMARELO" RESET " ligado.\n");
             gpio_put(LED_RED, 1);
             gpio_put(LED_GREEN, 1);
             state = 1;
             break;
-        case 1: // Amarelo ligado (vermelho + verde)
-            
+        case 1: 
+            // Verde ligado 
             printf("\nMudança de estado: " GREEN "VERDE" RESET " ligado.\n");
             gpio_put(LED_RED, 0);
             gpio_put(LED_GREEN, 1);
             state = 2;
             break;
-        case 2: // Verde ligado
+        case 2: // Vermelho ligado
             printf("\nMudança de estado: " RED "VERMELHO" RESET " ligado.\n");
             gpio_put(LED_RED, 1);
             gpio_put(LED_GREEN, 0);
             state = 0;
             break;
     }
-    return true; // Continua o temporizador
+    return true; 
 }
 
 int main() {
-    stdio_init_all(); // Inicializa a comunicação serial
+    stdio_init_all(); // Inicializando a comunicação serial
 
-    // Configura os pinos dos LEDs como saída
     gpio_init(LED_RED);
     gpio_set_dir(LED_RED, GPIO_OUT);
     gpio_init(LED_GREEN);
     gpio_set_dir(LED_GREEN, GPIO_OUT);
     
-    // Inicializa no estado correto (vermelho ligado)
+    // Inicializar no estado correto (vermelho ligado)
     gpio_put(LED_RED, 1);
     gpio_put(LED_GREEN, 0);
     
@@ -60,11 +59,11 @@ int main() {
     struct repeating_timer timer;
     add_repeating_timer_ms(3000, repeating_timer_callback, NULL, &timer);
 
-    // Loop principal
+
     while (true) {
-        seconds++; // Incrementa o contador de segundos
+        seconds++; 
         
-        // Imprime o LED que está ligado a cada segundo
+        // Imprimir o LED que está ligado a cada segundo
         switch (state) {
             case 0:
                 printf("(%d segundos) " RED "LED VERMELHO" RESET " está ligado.\n", seconds);
@@ -77,6 +76,6 @@ int main() {
                 break;
         }
 
-        sleep_ms(1000); // Aguarda 1 segundo antes da próxima impressão
+        sleep_ms(1000); 
     }
 }
